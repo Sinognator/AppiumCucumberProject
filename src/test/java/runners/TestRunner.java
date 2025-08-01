0b236d9e42a23f359cd2567ce094c94d4025e32c
paulo.rocha <pr.moreirarocha>
2025-08-01 18:22:46 -0300

    Corrigido yml para Automation8

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = {
                 "pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm:target/allure-results"
         },
         features = "src/test/resources/features/LoginSauce.feature",
         glue = {"steps"}
